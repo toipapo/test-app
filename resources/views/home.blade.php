a1e380850d81e1f2fa47486317a21baae2f78bbb
Gabbyiiel <gabbypaylaga@gmail.com>
2023-02-06 15:19:04 +0800

    Tae

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -17,7 +17,7 @@
         </div>
         <div class="form-floating">
           <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-          <label for="floatingqdasdInput">Email address</label>
+          <label for="floatingInput">Email address</label>
         </div>
       </form>
     </div>
